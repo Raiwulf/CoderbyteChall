c58128a1e99bf37641910a3153f514c80bee8596
Raiwulf <Raiwulf@msn.com>
2022-04-06 13:40:49 +0300

    Update FirstFactorial.js

--- FirstFactorial.js
+++ FirstFactorial.js
@@ -1,10 +1,9 @@
 function FirstFactorial(num) { 
+  
 for(var i=(num-1);i>1;i--){
   num=num*i
 }
-  // code goes here  
   return num; 
 }
-   
-// keep this function call here 
+
 console.log(FirstFactorial(readline()));
